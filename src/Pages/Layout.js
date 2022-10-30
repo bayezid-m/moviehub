34fe942105e9b038b14a0148fa8104c408ee7d12
Md Shayemur Rahman <sayemurrahman99@gmail.com>
2022-10-30 15:26:04 +0200

    basic categories designing

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -1,13 +1,14 @@
 import { Outlet, Link } from 'react-router-dom';
 import Form from '../components/Form';
+import Category from '../components/Category';
 
 const Layout = () => {
   return (
     <>
       <nav className='navbar'>
-        <h2 className="movie--title">MovieHub</h2>
+        <h2 className='movie--title'>MovieHub</h2>
         <Form />
-        <ul className="layout--list">
+        <ul className='layout--list'>
           <li>
             <Link to='/'>Home</Link>
           </li>
@@ -16,6 +17,8 @@ const Layout = () => {
           </li>
         </ul>
       </nav>
+
+      <Category />
 
       <Outlet />
     </>
